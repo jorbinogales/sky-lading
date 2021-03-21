5d81c08a5be9d242c7ca47e280523ea3334c68a3
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2021-03-21 18:15:12 -0400

    iowa

--- admin/database/factories/ContactFactory.php
+++ admin/database/factories/ContactFactory.php
@@ -7,10 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Contact::class, function (Faker $faker) {
     return [
+        'rif' => $faker->numberBetween(9999999, 999999999),
         'name' => $faker->name,
         'email' => $faker->safeEmail,
         'phone' => $faker->phoneNumber,
         'city' => $faker->city,
-        'city' => $faker->state,
+        'state' => $faker->state,
     ];
 });
